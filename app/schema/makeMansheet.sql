b86c8b0200f7225d345ceb3364b4e8dbcb59f137
arale8220 <36830946+arale8220@users.noreply.github.com>
2019-05-16 17:08:26 +0900

    Update makeMansheet.sql

--- app/schema/makeMansheet.sql
+++ app/schema/makeMansheet.sql
@@ -14,19 +14,24 @@ CREATE TABLE MGROUP(
 	Gid				int			NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     Gname			varchar(10) NOT NULL,
     Default_group	CHAR(1)		NOT NULL DEFAULT 'N',
-    Owner_uname		varchar(10)	NOT NULL FOREIGN KEY REFERENCES MUSER(Uname)
+    Owner_uname		varchar(10)	NOT NULL,
+    FOREIGN KEY(Owner_uname) REFERENCES MUSER(Uname)
 ) CHARSET=utf8;
 
-CREATE TABLE MUSER(
+CREATE TABLE PARTICIPATE(
     Uname		varchar(10) NOT NULL PRIMARY KEY,
-    Password	VARCHAR(40) NOT NULL
+    Gid         int         NOT NULL,
+    FOREIGN KEY(Uname) REFERENCES MUSER(Uname),
+    FOREIGN KEY(Gid) REFERENCES MGROUP(Gid)
 ) CHARSET=utf8;
 
 CREATE TABLE SCHEDULE(
     Start_time	varchar(16) NOT NULL,
     Description	VARCHAR(40),
-    Uname 		varchar(10) NOT NULL FOREIGN KEY REFERENCES MUSER(Uname),
-    Gid 		int 		NOT NULL FOREIGN KEY REFERENCES MGROUP(Gid)
+    Uname 		varchar(10) NOT NULL,
+    Gid 		int 		NOT NULL,
+    FOREIGN KEY(Uname) REFERENCES MUSER(Uname),
+    FOREIGN KEY(Gid) REFERENCES MGROUP(Gid)
 ) CHARSET=utf8;
 
 
