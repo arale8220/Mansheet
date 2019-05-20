5eaa8f271cedb530ed54558b46ac00a1c2abc05d
arale8220 <36830946+arale8220@users.noreply.github.com>
2019-05-20 18:34:36 +0900

    /user [get,delete] done

--- app/schema/makeMansheet.sql
+++ app/schema/makeMansheet.sql
@@ -16,14 +16,14 @@ CREATE TABLE MUSER(
 
 CREATE TABLE MGROUP(
 	Gid				int			NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    Gname			varchar(10) NOT NULL,
+    Gname			varchar(30) NOT NULL,
     Default_group	CHAR(1)		NOT NULL DEFAULT 'N',
     Owner_uname		varchar(10)	NOT NULL,
     FOREIGN KEY(Owner_uname) REFERENCES MUSER(Uname)
 ) CHARSET=utf8;
 
 CREATE TABLE PARTICIPATE(
-    Uname		varchar(10) NOT NULL PRIMARY KEY,
+    Uname		varchar(10) NOT NULL,
     Gid         int         NOT NULL,
     FOREIGN KEY(Uname) REFERENCES MUSER(Uname),
     FOREIGN KEY(Gid) REFERENCES MGROUP(Gid)
@@ -39,26 +39,53 @@ CREATE TABLE SCHEDULE(
     FOREIGN KEY(Gid) REFERENCES MGROUP(Gid)
 ) CHARSET=utf8;
 
-/* 
 
-DROP PROCEDURE IF EXISTS createMuser; 
-DELIMITER //
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS createMuser $$ 
 CREATE PROCEDURE createMuser(
-    IN _Uname VARCHAR(10), 
-    IN _Password VARCHAR(40)
+    IN  _Uname      VARCHAR(10), 
+    IN  _Password   VARCHAR(40),
+    OUT RESULT      INT
 )
 BEGIN
-    IF (select exists (select 1 from MUSER where Uname = _Uname)) THEN
-        select 'Username Exists!!';
-    ELSE
-        insert into MUSER values ( _Uname, _Password );
-    END IF;
-END
-//
+    DECLARE exit handler for SQLEXCEPTION
+    BEGIN
+        ROLLBACK;        
+        SET RESULT = 0;  
+    END;
+    INSERT INTO MUSER VALUES (_Uname, _Password);
+    START TRANSACTION;
+        INSERT INTO MGROUP (Gname, Default_group, Owner_uname) VALUES (_Uname, 'Y', _Uname);
+        INSERT INTO PARTICIPATE VALUES (_Uname, last_insert_id() );
+    COMMIT;
+    SET RESULT = 1;
+END $$
+
+
+
+DROP PROCEDURE IF EXISTS deleteMuser $$ 
+CREATE PROCEDURE deleteMuser(
+    IN  _Uname      VARCHAR(10), 
+    IN  _Password   VARCHAR(40),
+    OUT RESULT      INT
+)
+BEGIN
+    DECLARE exit handler for SQLEXCEPTION
+    BEGIN
+        ROLLBACK;        
+        SET RESULT = 0;  
+    END;
+    START TRANSACTION;
+        DELETE FROM SCHEDULE WHERE Uname=_Uname;
+        DELETE FROM PARTICIPATE WHERE Uname=_Uname;
+        DELETE FROM MGROUP WHERE Owner_uname=_Uname;
+        DELETE FROM MUSER WHERE Uname=_Uname;
+    COMMIT;
+    SET RESULT = 1;
+END $$
+
 DELIMITER ;
-
- */
-
 
 
 
--- newvenv/api.py
+++ newvenv/api.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from flask_restful import reqparse
 from flaskext.mysql import MySQL
 from flask import Response
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
@@ -29,17 +30,15 @@ class User(Resource):
             _Password = args['password']
 
             #닉네임 형식 확인
-            if (_Uname[2].isspace()) or (not (_Uname[0:2].isdigit())) or (len(_Uname)>10):
-                res = {'Message': "Please check your nickname. \nFirst two character must be digit. \nDo not write off digits and nickname.\nNickname must be less than 10 characters"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
-
+            if (len(_Uname)<4) or (not (_Uname[0:2].isdigit())) or (len(_Uname)>10) or (_Uname[2].isspace()) :
+                return user406Response("Please check your nickname. \nFirst two character must be digit. \nDo not write off digits and nickname.\nNickname must be less than 10 characters")
+                
             #비밀번호가 너무 짧거나 길지 않은지 확인
             if (len(_Password)<4) :
-                res = {'Message': "Password is too short. It must be longer than 4 characters"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+                return user406Response("Password is too short. It must be longer than 4 characters")
             if (len(_Password)>40) :
-                res = {'Message': "Password is too long. It must be less than 40 characters"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+                return user406Response("Password is too long. It must be less than 40 characters")
+            
 
             #이미 있는 유저의 아이디인지 확인
             conn = mysql.connect()
@@ -48,35 +47,23 @@ class User(Resource):
             cursor.execute(sql)
             already = cursor.fetchone()[0]
             if already :
-                res = {'Message': "User already exists"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+                return user406Response("User already exists")
 
-            #db에 유저 insert
-            sql = """insert into MUSER values ('""" + _Uname + """', '""" + _Password + """');"""
-            cursor.execute(sql)
-            conn.commit()
-
-            #유저의 default 그룹 생성
-            sql = """INSERT INTO MGROUP(Gname, Default_group, Owner_uname) 
-                    VALUES ( '""" + _Uname + """의 일정' , 'Y', '""" + _Uname + """');"""
-            cursor.execute(sql)
-            conn.commit()
-
-            #유저가 default 그룹에 들어가있다는 관계 추가
-            sql = """SELECT last_insert_id();"""
-            cursor.execute(sql)
-            last_insert_id = cursor.fetchone()[0] #fetchone은 1차원 튜플, fetchall은 2차원
-            sql = """INSERT INTO PARTICIPATE 
-                    VALUES ( '""" + _Uname + """' ,
-                            """ + str(last_insert_id) + """);"""
-            cursor.execute(sql)
-            conn.commit()
-
-            res = {'Message': "User created successfully. \nGo to mainpage for log-in"}
-            return Response(str(res).replace("'", "\""), status=201, mimetype='application/json')
+            #유저 추가 프로시져 실행
+            args = [_Uname, _Password, 0]
+            result_args = cursor.callproc('createMuser', args)
+            if result_args[2]:
+                return user20XResponse("User created successfully. \nGo to mainpage for log-in", \
+                        _Uname, _Password, 201)
+            else:
+                raise SQLError()
 
         except Exception as e:
-            return Response(str({'error': str(e)}), status=400, mimetype="application/json")
+            return error400Response(str(e))
+
+        finally:
+            cursor.close()
+            conn.close()
 
     def delete(self):
         try:
@@ -95,31 +82,26 @@ class User(Resource):
             cursor.execute(sql)
             existing = cursor.fetchone()
 
-            if existing is None: #존재하지 않는 아이디인 경우
-                res = {'Message': "User does not exists"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+            if (existing is None): #존재하지 않는 아이디인 경우
+                return user406Response("User does not exists")    
             if existing[1] != _Password: #옳지 않은 비밀번호를 입력한 경우
-                res = {'Message': "Please enter right password for secession"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
-
-            #유저 삭제
-            sql = """delete from SCHEDULE where Uname = '""" + _Uname + """';"""
-            cursor.execute(sql)
-            conn.commit()
-            sql = """delete from PARTICIPATE where Uname = '""" + _Uname + """';"""
-            cursor.execute(sql)
-            conn.commit()
-            sql = """delete from MGROUP where Owner_uname = '""" + _Uname + """';"""
-            cursor.execute(sql)
-            conn.commit()
-            sql = """delete from MUSER where Uname = '""" + _Uname + """';"""
-            cursor.execute(sql)
-            conn.commit()
-            res = {'Message': "User deleted successfully. \nGo to mainpage for log-in"}
-            return Response(str(res).replace("'", "\""), status=200, mimetype='application/json')
+                return user406Response("Please enter right password for secession")
+                
+            #유저 삭제 프로시져 실행
+            args = [_Uname, _Password, 0]
+            result_args = cursor.callproc('deleteMuser', args)
+            if result_args[2]:
+                return user20XResponse("User deleted successfully. \nGo to mainpage for log-in", \
+                        _Uname, _Password, 200)
+            else:
+                raise SQLError()
 
         except Exception as e:
-            return Response(str({'error': str(e)}), status=400, mimetype="application/json")
+            return error400Response(str(e))
+
+        finally:
+            cursor.close()
+            conn.close()
 
     def get(self):
         try:
@@ -138,13 +120,11 @@ class User(Resource):
             cursor.execute(sql)
             existing = cursor.fetchone()
 
-            if existing is None: #존재하지 않는 아이디인 경우
-                res = {'Message': "User does not exists"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+            if (existing is None): #존재하지 않는 아이디인 경우
+                return user406Response("User does not exists")    
             if existing[1] != _Password: #옳지 않은 비밀번호를 입력한 경우
-                res = {'Message': "Please enter right password for log-in"}
-                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
-
+                return user406Response("Please enter right password for secession")
+            
             #로그인 된 유저의 디폴트 그룹의 GID 가져오기
             conn = mysql.connect()
             cursor = conn.cursor()
@@ -161,12 +141,85 @@ class User(Resource):
             json_data=[]
             for one in schedules:
                 json_data.append(dict(zip(row_headers,one)))
-            return Response(str(json_data).replace("'", "\""), status=200, mimetype='application/json')
+
+            res = {'message' : "User logged in successfully.", \
+                'username' : _Uname, 'password' : _Password, \
+                'schedules' : json_data}
+
+            return Response(str(res).replace("'", "\""), status=200, mimetype='application/json')
 
         except Exception as e:
-            return Response(str({'error': str(e)}), status=400, mimetype="application/json")
+            return error400Response(str(e))
+
+        finally:
+            cursor.close()
+            conn.close()
+
+class GROUP(Resource):
+
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('username', type=str)
+            parser.add_argument('groupname', type=str)
+            args = parser.parse_args()
+
+            _Uname = args['username']
+            _Gname = args['groupname']
+
+            #그룹 이름 길이 확인
+            if (len(_Uname)<1) or (len(_Uname)>10):
+                res = {'message': "Group name is too short or too long"}
+                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+
+            #그룹 추가
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            sql = """INSERT INTO MGROUP(Gname, Owner_uname) 
+                    VALUES ( '""" + _Gname + """, '""" + _Uname + """');"""
+            cursor.execute(sql)
+            conn.commit()
+
+            #유저와 그룹 관계 추가
+            sql = """SELECT last_insert_id();"""
+            cursor.execute(sql)
+            last_insert_id = cursor.fetchone()[0] #fetchone은 1차원 튜플, fetchall은 2차원
+            sql = """INSERT INTO PARTICIPATE 
+                    VALUES ( '""" + _Uname + """' ,
+                            """ + str(last_insert_id) + """);"""
+            cursor.execute(sql)
+            conn.commit()
+
+            res = {'message': "Group created successfully."}
+            return Response(str(res).replace("'", "\""), status=201, mimetype='application/json')
+
+        except Exception as e:
+            return error400Response(str(e))
+        
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get(self):
+
+
+        
 
 api.add_resource(User, '/user')
+
+def user406Response(msg):
+    return Response(str({'message': msg}).replace("'", "\""), status=406, mimetype='application/json')
+
+def user20XResponse(msg, user, pw, state):
+    res = {'message' : msg, 'username' : user, 'password' : pw}
+    return Response(str(res).replace("'", "\""), status=state, mimetype='application/json')
+
+def error400Response(msg):
+    return Response(str({'error': msg}), status=400, mimetype="application/json")
+
+class SQLError(Exception):
+    def __str__(self):
+        return "There are some problem in SQL. It has been rolled back."
 
 if __name__ == '__main__':
     app.run(debug=True)
