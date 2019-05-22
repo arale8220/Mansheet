f5aca6781c3031ccac8520f20ea464e900d23f3a
Hou <tkdgh97531@kaist.ac.kr>
2019-05-23 05:49:39 +0900

    [dev-hou] Add schedule patch

--- newvenv/api.py
+++ newvenv/api.py
@@ -445,8 +445,57 @@ class SCHEDULE(Resource):
             cursor.close()
             conn.close()
     #update 시간표. Description 바꾸기
-    def update(self):
-        PATCH = 1
+    def patch(self):
+        try:
+            #과연 다른 정보들이 필요한가? Sid만 주면 안되남
+            parser = reqparse.RequestParser()
+            parser.add_argument('start_date', type=str)
+            parser.add_argument('start_time', type=str)
+            parser.add_argument('username', type=str)
+            parser.add_argument('groupname',type=str)
+            parser.add_argument('description', type=str)
+            parser.add_argument('duration', type=str)
+            parser.add_argument('sid', type=str)
+            args = parser.parse_args()
+
+            _startDate = args['start_date']
+            _startTime = args['start_time']
+            _Gname = args['groupname']
+            _Uname = args['username']
+            _Description = args['description'] or ''
+            _Duration = args['duration']
+            _Sid= args['sid']
+
+            #그룹 추가
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("""select Gid from SCHEDULE where Sid = '""" + str(_Sid) + """';""" ) 
+            fetchGid = cursor.fetchone()
+
+            print(fetchGid[0])
+
+            if (fetchGid[0]):
+                sql = """DELETE FROM SCHEDULE where Sid = '""" + str(_Sid) + """';"""
+            else :
+                res = {'message': "schedule does not exists "}
+                return Response(str(res).replace("'", "\""), status=406, mimetype='application/json')
+            cursor.execute(sql)
+            sql = """INSERT INTO SCHEDULE(Start_date,start_time,Uname,duration, Gid, description) 
+                    VALUES ( '""" + _startDate + """' , '""" + _startTime + """','""" + _Uname + """','""" + str(_Duration) + """', '""" + str(fetchGid[0]) + """', '""" + _Description + """');"""
+            cursor.execute(sql)
+
+
+            conn.commit()
+            res = {'message': "schedule changed successfully."}
+            return Response(str(res).replace("'", "\""), status=201, mimetype='application/json')
+
+        except Exception as e :
+            print(e)
+            return error400Response(str(e))
+
+        finally:
+            cursor.close()
+            conn.close()
     # 데이터 받아오기
     def get(self):
         GET = 1
@@ -477,11 +526,7 @@ class SCHEDULE(Resource):
             cursor = conn.cursor()
             cursor.execute("""select * from SCHEDULE where Sid = '""" + str(_Sid) + """';""" ) 
             fetchSid = cursor.fetchone()
-            
-            print(fetchSid)
-            
-            # return
-            
+
             if (fetchSid[0]):
                 sql = """DELETE FROM SCHEDULE where Sid = '""" + str(_Sid) + """';"""
             else :
